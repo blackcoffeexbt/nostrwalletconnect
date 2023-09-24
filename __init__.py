d371e75ba1ff09b5a9f35897210687a447f7dbc1
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-09-24 17:34:07 +0100

    Started on pay_invoice. Not sure why relays arent responding to REQ

--- __init__.py
+++ __init__.py
@@ -1,11 +1,23 @@
 import asyncio
+import json
 
 from fastapi import APIRouter
 from fastapi.staticfiles import StaticFiles
 
+from typing import List
+
+from asyncio import Task
+
+from lnbits.extensions.nostrclient.nostr.key import PrivateKey
+
+from loguru import logger
+
+from lnbits.extensions.nostrwalletconnect.nostr.NostrClient import NostrClient
+from lnbits.extensions.nostrwalletconnect.nostr.NostrEvent import NostrEvent
+
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.settings import settings
 
 db = Database("ext_nostrwalletconnect")
 
@@ -19,16 +31,54 @@ nostrwalletconnect_static_files = [
     }
 ]
 
-
 def nostrwalletconnect_renderer():
     return template_renderer(["lnbits/extensions/nostrwalletconnect/templates"])
 
 
-from .tasks import wait_for_paid_invoices
 from .views import *  # noqa: F401,F403
 from .views_api import *  # noqa: F401,F403
+from .tasks import *
+
 
 
 def nostrwalletconnect_start():
+    from lnbits.tasks import catch_everything_and_restart
+    from lnbits.app import settings
+
+    # TODO: un-hardcode this
+    wallet_connect_secret = "362805b0d6ebec963f098ca849e7228d4ece39d970d8f97af5bdccdecdc80ea1"
+
+    nostr_client = NostrClient()
+
+    scheduled_tasks: List[Task] = []
+
+    secret = settings.nostr_wallet_connect_secret
+    service_privkey_hex = "bdd19cecd942ed8964c2e0ddc92d5e09838d3a09ebb230d974868be00886704b"
+    pk = bytes.fromhex(service_privkey_hex)
+    private_key = PrivateKey(pk)
+    service_pubkey_hex = private_key.public_key.hex()
+
+    # every time lnbits starts, we need to send an info event to relays to inform the relay of
+    # the wallet connect service's capabilities
+    capabilities_event = get_service_capabilities_event(service_pubkey_hex)
+
+    async def _publish_capabilities_event():
+        await nostr_client.publish_nostr_event(capabilities_event)
+
+    async def _subscribe_to_nostr_request():
+        # wait for 'nostrclient' extension to initialize
+        await asyncio.sleep(10)
+        await nostr_client.run_forever()
+        raise ValueError("Must reconnect to websocket")
+
+    async def _wait_for_nostr_events():
+        # wait for this extension to initialize
+        await asyncio.sleep(15)
+        await wait_for_nostr_events(nostr_client, service_pubkey_hex)
+        # await asyncio.sleep(15)
+        # await _publish_capabilities_event()
+
     loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task1 = loop.create_task(catch_everything_and_restart(_subscribe_to_nostr_request))
+    task2 = loop.create_task(catch_everything_and_restart(_wait_for_nostr_events))
+    scheduled_tasks.extend([task1, task2])
--- tasks.py
+++ tasks.py
@@ -1,29 +1,64 @@
-# tasks.py is for asynchronous when invoices get paid
-
-# add your dependencies here
-
 import asyncio
-
+import json
 from loguru import logger
+from lnbits.extensions.nostrwalletconnect.nostr.NostrClient import NostrClient
+from lnbits.extensions.nostrwalletconnect.nostr.NostrEvent import NostrEvent
+from lnbits.extensions.nostrclient.nostr.event import EventKind
+from lnbits.extensions.nostrclient.nostr.key import PrivateKey
 
-from lnbits.core.models import Payment
-from lnbits.helpers import get_current_extension_name
-from lnbits.tasks import register_invoice_listener
+import secp256k1
 
+import time
 
-async def wait_for_paid_invoices():
-    invoice_queue = asyncio.Queue()
-    register_invoice_listener(invoice_queue, get_current_extension_name())
+def sign_message_hash(private_key: str, hash: bytes) -> str:
+    privkey = secp256k1.PrivateKey(bytes.fromhex(private_key))
+    sig = privkey.schnorr_sign(hash, None, raw=True)
+    return sig.hex()
+
+async def subscribe_to_wallet_service_requests(nostr_client: NostrClient, wallet_connect_service_pubkey: str):
+    # get env NOSTR_WALLET_CONNECT_PUBKEY value
+    logger.info("Subscribing to wallet service requests")
+    logger.info(f"Wallet service pubkey: {wallet_connect_service_pubkey}")
+
+    await nostr_client.subscribe_wallet_connect_client_requests(wallet_connect_service_pubkey)
+
+async def process_nostr_message( msg: str):
+    try:
+        type, *rest = json.loads(msg)
+
+        if type.upper() == "EVENT":
+            _, event = rest
+            event = NostrEvent(**event)
+            if event.kind == EventKind.WALLET_CONNECT_REQUEST:
+                logger.info(f"Wallet connect request received: {event.dict()}")
+            return
+
+    except Exception as ex:
+        logger.debug(ex)
+
+async def wait_for_nostr_events(nostr_client: NostrClient, wallet_connect_service_pubkey: str):
+    await subscribe_to_wallet_service_requests(nostr_client, wallet_connect_service_pubkey)
 
     while True:
-        payment = await invoice_queue.get()
-        await on_invoice_paid(payment)
+        message = await nostr_client.get_event()
+        logger.info(f"Received message: {message}")
+        await process_nostr_message(message)
 
+def get_service_capabilities_event(private_key_hex: str):
+    pk = bytes.fromhex(private_key_hex)
+    private_key = PrivateKey(pk)
+    public_key_hex = private_key.public_key.hex()
 
-async def on_invoice_paid(payment: Payment) -> None:
-    if (
-        payment.extra.get("tag") != "nostrwalletconnect"
-    ):  # Will grab any payment with the tag "nostrwalletconnect"
-        logger.debug(payment)
-        # Do something
-    return
+    event = NostrEvent(
+        pubkey=public_key_hex,
+        created_at=round(time.time()),
+        kind=EventKind.WALLET_CONNECT_INFO,
+        content="pay_invoice"
+    )
+    event.id = event.event_id
+
+    event.sig = sign_message_hash(private_key_hex, bytes.fromhex(event.id))
+
+    logger.info(f"Capabilities event: {event.dict()}")
+
+    return event
